6971dbbc11ebb20e599832574a773dee428af68d
Vest <Vest@users.noreply.github.com>
2021-03-22 14:11:39 +0100

    Added "render hex" sample with async support
    Signed-off-by: Vest <Vest@users.noreply.github.com>

--- src/renderhex.rs
+++ src/renderhex.rs
@@ -9,8 +9,6 @@ use crossbeam::channel::unbounded;
 
 use crate::Operation::*;
 use crate::Orientation::*;
-use svg::save;
-use core::num::dec2flt::parse::Sign::Positive;
 
 const WIDTH: isize = 400;
 const HEIGHT: isize = WIDTH;
@@ -47,7 +45,65 @@ struct Artist {
     heading: Orientation,
 }
 
-impl Artist {}
+impl Artist {
+    fn new() -> Artist {
+        Artist {
+            heading: North,
+            x: HOME_X,
+            y: HOME_Y,
+        }
+    }
+
+    fn home(&mut self) {
+        self.x = HOME_X;
+        self.y = HOME_Y;
+    }
+
+    fn forward(&mut self, distance: isize) {
+        match self.heading {
+            North => self.y += distance,
+            South => self.y -= distance,
+            West => self.x += distance,
+            East => self.x -= distance,
+        }
+    }
+
+    fn turn_right(&mut self) {
+        self.heading = match self.heading {
+            North => East,
+            South => West,
+            West => North,
+            East => South,
+        }
+    }
+
+    fn turn_left(&mut self) {
+        self.heading = match self.heading {
+            North => West,
+            South => East,
+            West => South,
+            East => North,
+        }
+    }
+
+    fn wrap(&mut self) {
+        if self.x < 0 {
+            self.x = HOME_X;
+            self.heading = West;
+        } else if self.x > WIDTH {
+            self.x = HOME_X;
+            self.heading = East;
+        }
+
+        if self.y < 0 {
+            self.y = HOME_Y;
+            self.heading = North;
+        } else if self.y > HEIGHT {
+            self.y = HOME_Y;
+            self.heading = South;
+        }
+    }
+}
 
 fn parse_byte(byte: u8) -> Operation {
     match byte {
@@ -120,6 +176,38 @@ fn convert(operations: &Vec<Operation>) -> Vec<Command> {
         turtle.wrap();
     }
     path_data
+}
+
+fn generate_svg(path_data: Vec<Command>) -> Document {
+    let background = Rectangle::new()
+        .set("x", 0)
+        .set("y", 0)
+        .set("width", WIDTH)
+        .set("height", HEIGHT)
+        .set("fill", "#ffffff");
+
+    let border = background.clone()
+        .set("fill-opacity", "0.0")
+        .set("stroke", "#cccccc")
+        .set("stroke-width", 3 * STROKE_WIDTH);
+
+    let sketch = Path::new()
+        .set("fill", "none")
+        .set("stroke", "#2f2f2f")
+        .set("stroke-width", STROKE_WIDTH)
+        .set("stroke-opacity", 0.9)
+        .set("d", Data::from(path_data));
+
+    let document = Document::new()
+        .set("viewBox", (0, 0, WIDTH, HEIGHT))
+        .set("width", WIDTH)
+        .set("height", HEIGHT)
+        .set("style", "outline: 5px solid #800000")
+        .add(background)
+        .add(sketch)
+        .add(border);
+
+    document
 }
 
 fn main() {
